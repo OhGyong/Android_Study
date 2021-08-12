a1ee58084b219794e0f22b48e19d7e644794a501
OhGyong <oky7143@naver.com>
2021-08-12 21:41:17 +0900

    앱 아이콘 및 테마 색 변경

--- Android Kotlin Basics in Kotlin/Unit 2- Layouts/PATHWAY 1-Get user input in an app Part1/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Android Kotlin Basics in Kotlin/Unit 2- Layouts/PATHWAY 1-Get user input in an app Part1/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -15,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
     }
 
     // UI를 확인하고 팁을 계산하는 메서드
@@ -45,5 +52,17 @@ class MainActivity : AppCompatActivity() {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
 
+    }
+
+    // 키보드 내리는 메서드
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
