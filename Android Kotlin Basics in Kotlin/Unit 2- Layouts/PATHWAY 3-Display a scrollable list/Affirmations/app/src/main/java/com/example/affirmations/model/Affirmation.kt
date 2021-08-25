37a9b0ccb39d99c2ad9d7ff00fea085aa5cd8c13
OhGyong <oky7143@naver.com>
2021-08-25 14:48:52 +0900

    안드로이드 코틀린 강의 레퍼지토리 이동

--- Android Kotlin Basics in Kotlin/Unit 2- Layouts/PATHWAY 3-Display a scrollable list/Affirmations/app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++
@@ -1,9 +0,0 @@
-package com.example.affirmations.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-// Affirmation 인스턴스를 만들 때 확인 문자열의 리소스 ID를 전달해야 한다. 이때 ID 값은 정수이다.
-data class Affirmation(@StringRes val stringResourceId: Int,
-                       @DrawableRes val imageResourceId: Int
-)
