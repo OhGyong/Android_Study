d12df8c7afc6fd200c12bfeecf1d4df2079175b5
OhGyong <oky7143@naver.com>
2022-01-27 16:38:38 +0900

    네비게이션 삭제하고 FragmentContainerView만 사용

--- RecyclerView with Retrofit (MVVM)/RecyclerView_with_Retrofit/app/src/main/java/com/example/recyclerview_with_retrofit/MainActivity.kt
+++ RecyclerView with Retrofit (MVVM)/RecyclerView_with_Retrofit/app/src/main/java/com/example/recyclerview_with_retrofit/MainActivity.kt
@@ -7,15 +7,8 @@ import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
     }
 }
