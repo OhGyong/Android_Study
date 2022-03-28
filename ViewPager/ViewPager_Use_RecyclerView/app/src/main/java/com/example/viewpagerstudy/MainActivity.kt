4f506b9e5b256d299ba019ea2a64983186363a37
OhGyong <oky7143@naver.com>
2022-03-28 16:10:59 +0900

    ViewPager_Use_RecyclerView 폴더 이동

--- ViewPager/ViewPager_Use_RecyclerView/app/src/main/java/com/example/viewpagerstudy/MainActivity.kt
+++ ViewPager/ViewPager_Use_RecyclerView/app/src/main/java/com/example/viewpagerstudy/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.viewpagerstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        viewPager.adapter = ViewPagerAdapter(imageList())
+
+
+    }
+
+    private fun imageList(): ArrayList<Int>{
+        return arrayListOf(R.drawable.intro1, R.drawable.intro2)
     }
 }
---
+++ ViewPager/ViewPager_Use_RecyclerView/app/src/main/java/com/example/viewpagerstudy/ViewPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.viewpagerstudy
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ViewPagerAdapter(imageList: ArrayList<Int>): RecyclerView.Adapter<ViewPagerAdapter.PagerViewHolder>() {
+
+    var item = imageList
+
+    inner class PagerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var image: ImageView = itemView.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.image_item, parent, false)
+        return PagerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+        holder.image.setImageResource(item[position])
+    }
+
+    override fun getItemCount(): Int {
+        return item.size
+    }
+}
