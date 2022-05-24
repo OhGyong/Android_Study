4788f8fdba9f9377f2be360e78eec80b24c783ae
OhGyong <oky7143@naver.com>
2022-05-24 23:54:31 +0900

    TabLayout과 한개의 Fragment 사용. 텍스트가 겹쳐서 보이는 문제 발생

--- TabLayout/TabLayoutWithOneFragment/app/src/main/java/com/example/tablayoutwithonefragment/MainActivity.kt
+++ TabLayout/TabLayoutWithOneFragment/app/src/main/java/com/example/tablayoutwithonefragment/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.tablayoutwithonefragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tablayoutwithonefragment.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        setTabLayout()
+    }
+
+    private fun setTabLayout() {
+        mBinding.tabMain.addTab(mBinding.tabMain.newTab().setText("첫번째"))
+        mBinding.tabMain.addTab(mBinding.tabMain.newTab().setText("두번째"))
+        mBinding.tabMain.addTab(mBinding.tabMain.newTab().setText("세번째"))
+        mBinding.tabMain.addTab(mBinding.tabMain.newTab().setText("네번째"))
+
+        val bundle = Bundle()
+        val fragment = MainFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        bundle.putString("data", "첫번째")
+        fragment.arguments = bundle
+        transaction.add(mBinding.flMain.id, fragment)
+        transaction.commit()
+
+
+        mBinding.tabMain.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                bundle.putString("data", tab!!.text.toString())
+                val fragment2 = MainFragment()
+                fragment2.arguments = bundle
+
+                val transaction2 = supportFragmentManager.beginTransaction()
+                transaction2.add(mBinding.flMain.id, fragment2)
+                transaction2.commit()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
+
     }
 }
---
+++ TabLayout/TabLayoutWithOneFragment/app/src/main/java/com/example/tablayoutwithonefragment/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.example.tablayoutwithonefragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tablayoutwithonefragment.databinding.FragmentMainBinding
+
+class MainFragment: Fragment() {
+    private lateinit var mBinding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentMainBinding.inflate(inflater, container, false)
+
+        mBinding.tvFragment.text = arguments?.getString("data")
+
+        return mBinding.root
+    }
+}
