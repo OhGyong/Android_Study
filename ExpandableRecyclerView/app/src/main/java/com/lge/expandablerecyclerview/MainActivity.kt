bd43f22a86a41510c38a14c827bca5beb20e58cf
OhGyong <ogyong7143@gmail.com>
2022-07-14 15:57:05 +0900

    ExpadnableRecyclerView 작업

--- ExpandableRecyclerView/app/src/main/java/com/lge/expandablerecyclerview/MainActivity.kt
+++ ExpandableRecyclerView/app/src/main/java/com/lge/expandablerecyclerview/MainActivity.kt
@@ -2,10 +2,53 @@ package com.lge.expandablerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.lge.expandablerecyclerview.databinding.ActivityMainBinding
+
+data class SampleData(
+    val title: String,
+    val date: String,
+    val content: String,
+    var expandable: Boolean = false
+)
 
 class MainActivity : AppCompatActivity() {
+
+    private var sampleList = arrayListOf(
+        SampleData(
+            "샘플 제목3",
+            "2022.04.11",
+            "본문입니다." +
+                    "본문입니다." +
+                    "본문입니다." +
+                    "본문입니다."
+        ),
+        SampleData(
+            "샘플 제목2",
+            "2022.03.23",
+            "본문입니다." +
+                    "본문입니다." +
+                    "본문입니다." +
+                    "본문입니다."
+        ),
+        SampleData(
+            "샘플 제목1",
+            "2022.03.18",
+            "본문입니다." +
+                    "본문입니다." +
+                    "본문입니다." +
+                    "본문입니다."
+        )
+    )
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.rvMain.adapter = MainListAdapter(sampleList)
+        mBinding.rvMain.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ ExpandableRecyclerView/app/src/main/java/com/lge/expandablerecyclerview/MainListAdapter.kt
@@ -0,0 +1,42 @@
+package com.lge.expandablerecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lge.expandablerecyclerview.databinding.ListItemRecyclerviewBinding
+
+class MainListAdapter(private var sampleDataList: ArrayList<SampleData>) :
+    RecyclerView.Adapter<MainListAdapter.ViewHolder>() {
+
+    inner class ViewHolder(private val binding: ListItemRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(mSampleData: SampleData){
+            binding.tvTitle.text = mSampleData.title
+            binding.tvDate.text = mSampleData.date
+            binding.tvContent.text = mSampleData.content
+
+            binding.ctParent.setOnClickListener {
+                if(mSampleData.expandable){
+                    binding.ctChild.visibility = View.GONE
+                    mSampleData.expandable = false
+                }else{
+                    binding.ctChild.visibility = View.VISIBLE
+                    mSampleData.expandable = true
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ListItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(sampleDataList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return sampleDataList.size
+    }
+}
