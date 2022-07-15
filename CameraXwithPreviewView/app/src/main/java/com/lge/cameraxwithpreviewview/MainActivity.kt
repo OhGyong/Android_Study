860eca2f5a5777c989053947cda6b8dff5bdd14a
OhGyong <ogyong7143@gmail.com>
2022-07-15 09:13:25 +0900

    CameraX with PreviewView 프로젝트 생성

---
+++ CameraXwithPreviewView/app/src/main/java/com/lge/cameraxwithpreviewview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lge.cameraxwithpreviewview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
