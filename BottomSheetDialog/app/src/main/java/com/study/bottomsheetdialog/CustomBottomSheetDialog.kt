efc2286578f7c274e79bc2914bf063be8990b6a0
OhGyong <ogyong7143@gmail.com>
2022-07-18 10:51:56 +0900

    필요없는 코드 삭제

--- BottomSheetDialog/app/src/main/java/com/study/bottomsheetdialog/CustomBottomSheetDialog.kt
+++ BottomSheetDialog/app/src/main/java/com/study/bottomsheetdialog/CustomBottomSheetDialog.kt
@@ -3,7 +3,6 @@ package com.study.bottomsheetdialog
 import android.content.Context
 import android.os.Bundle
 import android.view.Window
-import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.study.bottomsheetdialog.databinding.DialogBottomSheetBinding
 
@@ -27,7 +26,6 @@ class CustomBottomSheetDialog(context: Context) :
 
     class Builder(context: Context) {
         private val mDialog = CustomBottomSheetDialog(context)
-        private val mContext = context
 
         fun setTitle(text: String): Builder {
             text.let{
