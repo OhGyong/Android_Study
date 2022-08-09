4c2075ccfa5bfdbb267513a16859ff79d2840452
OhGyong <ogyong7143@gmail.com>
2022-08-09 18:00:04 +0900

    PermissionProcessing으로 프로젝트 명 변경

--- PermissionProcessing/app/src/main/java/com/study/permissionprocessing/MainActivity.kt
+++ PermissionProcessing/app/src/main/java/com/study/permissionprocessing/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.study.permissionnotagain
+package com.study.permissionprocessing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- PermissionProcessing/app/src/test/java/com/study/permissionprocessing/ExampleUnitTest.kt
+++ PermissionProcessing/app/src/test/java/com/study/permissionprocessing/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.study.permissionnotagain
+package com.study.permissionprocessing
 
 import org.junit.Test
 
