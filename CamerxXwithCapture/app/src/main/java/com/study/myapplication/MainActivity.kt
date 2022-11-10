18a254ea7888b23e1e0328ee4c5609e77404b6f7
OhGyong <ogyong7143@gmail.com>
2022-11-10 11:44:25 +0900

    - 버전 업데이트 - 카메라 권한 등록 - CameraX 등록 - CameraX, PreviewView 코드 작성

--- CamerxXwithCapture/app/src/main/java/com/study/myapplication/MainActivity.kt
+++ CamerxXwithCapture/app/src/main/java/com/study/myapplication/MainActivity.kt
@@ -1,11 +1,116 @@
-package com.study.myapplication
+package com.study.cameraxwithpreviewview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraControl
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.study.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private const val REQUIRED_PERMISSIONS = Manifest.permission.CAMERA
+    }
+
+    private lateinit var mBinding: ActivityMainBinding
+    private var camera: Camera? = null
+    private var cameraController: CameraControl? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        // 카메라 권한 확인
+        if (allPermissionsGranted()) {
+            startCamera() // 카메라 실행
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(REQUIRED_PERMISSIONS), REQUEST_CODE_PERMISSIONS)
+        }
     }
+
+    /**
+     * 카메라 실행
+     */
+    private fun startCamera() {
+
+        // 1. CameraProvider 요청
+        // ProcessCameraProvider는 Camera의 생명주기를 LifeCycleOwner의 생명주기에 Binding 함
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // 2. CameraProvier 사용 가능 여부 확인
+            // 생명주기에 binding 할 수 있는 ProcessCameraProvider 객체 가져옴
+            val cameraProvider = cameraProviderFuture.get()
+
+            // 3. 카메라를 선택하고 use case를 같이 생명주기에 binding
+
+            // 3-1. Preview를 생성 → Preview를 통해서 카메라 미리보기 화면을 구현.
+            // surfaceProvider는 데이터를 받을 준비가 되었다는 신호를 카메라에게 보내준다.
+            // setSurfaceProvider는 PreviewView에 SurfaceProvider를 제공해준다.
+            val preview = Preview.Builder().build()
+            preview.setSurfaceProvider(mBinding.viewFinder.surfaceProvider)
+            // 아래처럼 써도 됨
+//           val preview = Preview.Builder().build().also {
+//               it.setSurfaceProvider(mBinding.viewFinder.surfaceProvider)
+//           }
+
+            // 3-2. 카메라 세팅을 한다. (useCase는 bindToLifecycle에서)
+            // CameraSelector는 카메라 세팅을 맡는다.(전면, 후면 카메라)
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // binding 전에 binding 초기화
+                cameraProvider.unbindAll()
+
+                // 3-3. use case와 카메라를 생명 주기에 binding
+                camera = cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+                cameraController = camera!!.cameraControl
+                cameraController!!.setZoomRatio(1F) // 1x Zoom
+            } catch (exc: Exception) {
+                println("에러 $exc")
+            }
+
+            // 4. Preview를 PreviewView에 연결한다.
+            // surfaceProvider는 데이터를 받을 준비가 되었다는 신호를 카메라에게 보내준다.
+            // setSurfaceProvider는 PreviewView에 SurfaceProvider를 제공해준다.
+            preview.setSurfaceProvider(mBinding.viewFinder.surfaceProvider)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    /**
+     * 권한 요청 결과를 판단(requestPermissions에 의해 호출)
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults:
+        IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "접근 권한이 허용되지 않아 카메라를 실행할 수 없습니다. 설정에서 접근 권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    /**
+     * 카메라 권한 체크
+     */
+    private fun allPermissionsGranted() = ContextCompat.checkSelfPermission(
+        baseContext, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
 }
