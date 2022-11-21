d0c221a2acffcb40ed2c56e1540443fb779f4df5
OhGyong <ogyong7143@gmail.com>
2022-11-21 12:32:43 +0900

    - SearchListAdapter 생성

---
+++ SearchListwithSharedPreferences/app/src/main/java/com/study/searchlistwithsharedpreferences/SearchListAdapter.kt
@@ -0,0 +1,35 @@
+package com.study.searchlistwithsharedpreferences
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.study.searchlistwithsharedpreferences.databinding.ItemSearchBinding
+
+class SearchListAdapter: RecyclerView.Adapter<SearchListAdapter.ViewHolder>() {
+    private var itemList: ArrayList<PrefData> = arrayListOf()
+
+    inner class ViewHolder(private val mBinding: ItemSearchBinding) : RecyclerView.ViewHolder(mBinding.root) {
+        fun bind(mItem: PrefData) {
+            mBinding.tvSearchText.text = mItem.text
+            mBinding.tvSearchDate.text = mItem.date
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemSearchBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(itemList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+}
