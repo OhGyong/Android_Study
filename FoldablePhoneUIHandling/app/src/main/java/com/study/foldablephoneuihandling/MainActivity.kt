834640ed77c7e44414f9922d0161a87024e88533
OhGyong <ogyong7143@gmail.com>
2022-11-22 16:21:23 +0900

    - 기기 타입에 맞춰 버튼 크기 변환

--- FoldablePhoneUIHandling/app/src/main/java/com/study/foldablephoneuihandling/MainActivity.kt
+++ FoldablePhoneUIHandling/app/src/main/java/com/study/foldablephoneuihandling/MainActivity.kt
@@ -2,6 +2,8 @@ package com.study.foldablephoneuihandling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.ViewGroup
 import com.study.foldablephoneuihandling.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +12,18 @@ class MainActivity : AppCompatActivity() {
 
     private var deviceType = "NORMAL" // 기기 타입 저장 변수
 
+    private lateinit var llLayoutParams1: ViewGroup.LayoutParams
+    private lateinit var llLayoutParams2: ViewGroup.LayoutParams
+    private lateinit var llLayoutParams3: ViewGroup.LayoutParams
+    private lateinit var llLayoutParams4: ViewGroup.LayoutParams
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
         getDeviceType()
+        setBtnSize()
     }
 
     /**
@@ -41,5 +49,66 @@ class MainActivity : AppCompatActivity() {
                     "NORMAL"
                 }
             }
+    }
+
+    /**
+     * 버튼 크기 정하기
+     */
+    private fun setBtnSize() {
+
+        llLayoutParams1 = mBinding.ll1.layoutParams
+        llLayoutParams2 = mBinding.ll2.layoutParams
+        llLayoutParams3 = mBinding.ll3.layoutParams
+        llLayoutParams4 = mBinding.ll4.layoutParams
+
+        when(deviceType) {
+            "MINI" -> {
+                setLayoutParams(llLayoutParams1, 60F)
+                setLayoutParams(llLayoutParams2, 60F)
+                setLayoutParams(llLayoutParams3, 60F)
+                setLayoutParams(llLayoutParams4, 60F)
+            }
+            "NORMAL" -> {
+                setLayoutParams(llLayoutParams1, 75F)
+                setLayoutParams(llLayoutParams2, 75F)
+                setLayoutParams(llLayoutParams3, 75F)
+                setLayoutParams(llLayoutParams4, 75F)
+            }
+            "TABLET" -> {
+                setLayoutParams(llLayoutParams1, 90F)
+                setLayoutParams(llLayoutParams2, 90F)
+                setLayoutParams(llLayoutParams3, 90F)
+                setLayoutParams(llLayoutParams4, 90F)
+            }
+            else -> {
+                setLayoutParams(llLayoutParams1, 75F)
+                setLayoutParams(llLayoutParams2, 75F)
+                setLayoutParams(llLayoutParams3, 75F)
+                setLayoutParams(llLayoutParams4, 75F)
+            }
+        }
+
+        mBinding.ll1.layoutParams = llLayoutParams1
+        mBinding.ll2.layoutParams = llLayoutParams2
+        mBinding.ll3.layoutParams = llLayoutParams3
+        mBinding.ll4.layoutParams = llLayoutParams4
+    }
+
+
+    /**
+     * LinearLayout의 크기 변환
+     */
+    private fun setLayoutParams(llLayoutParams: ViewGroup.LayoutParams, value: Float) {
+        llLayoutParams.width = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            value,
+            resources?.displayMetrics
+        ).toInt()
+
+        llLayoutParams.height = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            value,
+            resources?.displayMetrics
+        ).toInt()
     }
 }
