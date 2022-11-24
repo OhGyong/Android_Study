e0be4eabcc645251e1bca5fe13a8b9cf2584d55f
OhGyong <ogyong7143@gmail.com>
2022-11-24 09:52:20 +0900

    - 버튼 클릭 시 가로, 세로 화면 회전 동작 추가

--- Screen Orientation/app/src/main/java/com/study/screenorientation/MainActivity.kt
+++ Screen Orientation/app/src/main/java/com/study/screenorientation/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.study.screenorientation
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn_main)
+
+        btn.setOnClickListener {
+            requestedOrientation = if(resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT){
+                ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            }else {
+                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            }
+        }
     }
 }
