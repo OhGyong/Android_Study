c13df00fee241f16e89aaec4ef453d2ba791c1b5
OhGyong <ogyong7143@gmail.com>
2022-12-14 14:09:19 +0900

    DrawerLayout 적용

--- DrawerLayout/app/src/main/java/com/study/drawerlayout/MainActivity.kt
+++ DrawerLayout/app/src/main/java/com/study/drawerlayout/MainActivity.kt
@@ -2,10 +2,14 @@ package com.study.drawerlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.study.drawerlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
 }
