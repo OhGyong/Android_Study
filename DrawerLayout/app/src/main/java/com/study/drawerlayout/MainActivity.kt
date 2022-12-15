70def513c97b904d813acd5cf5a0ab201cb873d5
OhGyong <ogyong7143@gmail.com>
2022-12-15 11:36:13 +0900

    - 버튼 추가 - drawer 동작 처리

--- DrawerLayout/app/src/main/java/com/study/drawerlayout/MainActivity.kt
+++ DrawerLayout/app/src/main/java/com/study/drawerlayout/MainActivity.kt
@@ -2,6 +2,9 @@ package com.study.drawerlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.study.drawerlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        // drawer 잠금
+        mBinding.dlMain.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+
+        // 열기 버튼
+        mBinding.btnMain.setOnClickListener {
+            mBinding.dlMain.open()
+//            mBinding.dlMain.openDrawer(GravityCompat.START)
+//            mBinding.dlMain.openDrawer(Gravity.LEFT)
+        }
+
+        // 닫기 버튼
+        mBinding.inViewDrawer.ivDrawerClose.setOnClickListener {
+            mBinding.dlMain.close()
+//            mBinding.dlMain.closeDrawer(GravityCompat.START)
+//            mBinding.dlMain.closeDrawer(Gravity.LEFT)
+        }
     }
 }
