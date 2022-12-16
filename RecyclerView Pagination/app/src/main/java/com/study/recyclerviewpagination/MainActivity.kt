eb5bcd8c0e91d0f8a2ccf022fc60135c5cc5893a
OhGyong <ogyong7143@gmail.com>
2022-12-16 15:06:02 +0900

    기능 동작 처리

--- RecyclerView Pagination/app/src/main/java/com/study/recyclerviewpagination/MainActivity.kt
+++ RecyclerView Pagination/app/src/main/java/com/study/recyclerviewpagination/MainActivity.kt
@@ -2,10 +2,54 @@ package com.study.recyclerviewpagination
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.study.recyclerviewpagination.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding : ActivityMainBinding
+    private lateinit var mAdapter : ActivityAdapter
+    private var page = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setAdapter()
+
+        val sampleList = resources.getStringArray(R.array.first_page).toList() as ArrayList<String>
+        mAdapter.setList(sampleList)
+
+        mBinding.rvMain.addOnScrollListener(object: RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                // 리사이클러뷰 아이템 위치 찾기, 아이템 위치가 완전히 보일때 호출됨
+                val rvPosition =
+                    (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition()
+
+                // 리사이클러뷰 아이템 총개수 (index 접근 이기 때문에 -1)
+                val totalCount =
+                    recyclerView.adapter?.itemCount?.minus(1)
+
+                if(rvPosition == totalCount) {
+                    page++
+                    if(page == 2) {
+                        sampleList.addAll(resources.getStringArray(R.array.second_page).toList() as ArrayList<String>)
+                        mAdapter.setList(resources.getStringArray(R.array.second_page).toList() as ArrayList<String>)
+                    }else if (page ==3) {
+                        sampleList.addAll(resources.getStringArray(R.array.third_page).toList() as ArrayList<String>)
+                        mAdapter.setList(resources.getStringArray(R.array.third_page).toList() as ArrayList<String>)
+                    }
+                    println(sampleList)
+                }
+            }
+        })
+    }
+
+    private fun setAdapter() {
+        mAdapter = ActivityAdapter()
+        mBinding.rvMain.adapter = mAdapter
     }
 }
