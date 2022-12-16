22a0f4c68e9aa20429207d1b9477a1de2549d0f0
OhGyong <ogyong7143@gmail.com>
2022-12-16 14:05:27 +0900

    Adapter 생성

---
+++ RecyclerView Pagination/app/src/main/java/com/study/recyclerviewpagination/ActivityAdapter.kt
@@ -0,0 +1,39 @@
+package com.study.recyclerviewpagination
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.study.recyclerviewpagination.databinding.ListItemMainBinding
+
+class ActivityAdapter: RecyclerView.Adapter<ActivityAdapter.ViewHolder>() {
+    private val mList = ArrayList<String>()
+
+
+    inner class ViewHolder(private val mBinding : ListItemMainBinding): RecyclerView.ViewHolder(mBinding.root) {
+        fun bind(listData: String) {
+            mBinding.tvItem.text = listData
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ListItemMainBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(mList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    fun setList(notifyList: ArrayList<String>) {
+        mList.addAll(notifyList)
+        notifyItemRangeChanged(0, mList.size)
+    }
+}
