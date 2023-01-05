04991b5e7ada7ec990318cd4db55f7d22d921fad
OhGyong <ogyong7143@gmail.com>
2023-01-05 16:28:28 +0900

    Room 파일 생성

---
+++ RecyclerView Pagination Remove Item/app/src/main/java/com/study/recyclerviewpaginationremoveitem/data/SampleData.kt
@@ -0,0 +1,4 @@
+package com.study.recyclerviewpaginationremoveitem.data
+
+class SampleData {
+}
---
+++ RecyclerView Pagination Remove Item/app/src/main/java/com/study/recyclerviewpaginationremoveitem/data/SampleDatabase.kt
@@ -0,0 +1,5 @@
+package com.study.recyclerviewpaginationremoveitem.data
+
+class SampleDatabase {
+
+}
