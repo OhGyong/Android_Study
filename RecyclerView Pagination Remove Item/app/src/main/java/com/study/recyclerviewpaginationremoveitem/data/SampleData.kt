96745e714149a7f62313dcecd219269a67cc8a8f
OhGyong <ogyong7143@gmail.com>
2023-01-05 17:45:01 +0900

    Room 설정 작업

--- RecyclerView Pagination Remove Item/app/src/main/java/com/study/recyclerviewpaginationremoveitem/data/SampleData.kt
+++ RecyclerView Pagination Remove Item/app/src/main/java/com/study/recyclerviewpaginationremoveitem/data/SampleData.kt
@@ -1,4 +1,13 @@
 package com.study.recyclerviewpaginationremoveitem.data
 
-class SampleData {
+import androidx.room.Entity
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "sample")
+data class SampleData(
+    @ColumnInfo(name = "title") val title: String
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
 }
--- RecyclerView Pagination Remove Item/app/src/main/java/com/study/recyclerviewpaginationremoveitem/data/SampleDatabase.kt
+++ RecyclerView Pagination Remove Item/app/src/main/java/com/study/recyclerviewpaginationremoveitem/data/SampleDatabase.kt
@@ -1,5 +1,8 @@
 package com.study.recyclerviewpaginationremoveitem.data
 
-class SampleDatabase {
+import androidx.room.Database
 
+@Database(entities = [SampleData::class], version = 1)
+abstract class SampleDatabase {
+    abstract fun gridViewDao() : SampleDao
 }
