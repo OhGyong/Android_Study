30972b8791bade4b9ac3fa0551cd03b1cce69e70
OhGyong <ogyong7143@gmail.com>
2023-01-06 09:52:19 +0900

    SampleDatabase 코드 수정

--- RecyclerView Pagination Remove Item/app/src/main/java/com/study/recyclerviewpaginationremoveitem/data/SampleDatabase.kt
+++ RecyclerView Pagination Remove Item/app/src/main/java/com/study/recyclerviewpaginationremoveitem/data/SampleDatabase.kt
@@ -1,8 +1,9 @@
 package com.study.recyclerviewpaginationremoveitem.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [SampleData::class], version = 1)
-abstract class SampleDatabase {
+abstract class SampleDatabase: RoomDatabase() {
     abstract fun gridViewDao() : SampleDao
 }
