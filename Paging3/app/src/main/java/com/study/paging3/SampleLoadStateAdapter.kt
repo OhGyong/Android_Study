bf9060369938920d9aeaa809486edf3beddec6b9
OhGyong <ogyong7143@gmail.com>
2023-01-18 10:20:09 +0900

    SampleLoadStateViewHolder를 Apapter 안으로 옮김

--- Paging3/app/src/main/java/com/study/paging3/SampleLoadStateAdapter.kt
+++ Paging3/app/src/main/java/com/study/paging3/SampleLoadStateAdapter.kt
@@ -1,15 +1,32 @@
 package com.study.paging3
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.paging.LoadState
 import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.study.paging3.databinding.ListItemFooterBinding
 
-class SampleLoadStateAdapter : LoadStateAdapter<SampleLoadStateViewHolder>() {
+class SampleLoadStateAdapter : LoadStateAdapter<SampleLoadStateAdapter.SampleLoadStateViewHolder>() {
+
+    inner class SampleLoadStateViewHolder(
+        private val binding: ListItemFooterBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(loadState: LoadState) {
+            binding.pbLoading.isVisible = loadState is LoadState.Loading
+        }
+    }
+
     override fun onBindViewHolder(holder: SampleLoadStateViewHolder, loadState: LoadState) {
         holder.bind(loadState)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): SampleLoadStateViewHolder {
-        return SampleLoadStateViewHolder.create(parent)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_footer, parent, false)
+        val binding = ListItemFooterBinding.bind(view)
+        return SampleLoadStateViewHolder(binding)
     }
 }
