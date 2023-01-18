217691fe06f1eba907c2d1392e8679a1229fb69a
OhGyong <ogyong7143@gmail.com>
2023-01-18 15:10:58 +0900

    필요 없는 쿼리 삭제

--- Paging3/app/src/main/java/com/study/paging3/data/SampleDao.kt
+++ Paging3/app/src/main/java/com/study/paging3/data/SampleDao.kt
@@ -6,12 +6,6 @@ import androidx.room.Query
 @Dao
 interface SampleDao {
     /**
-     * 리스트 사이즈 호출
-     */
-    @Query("SELECT COUNT(*) FROM sample")
-    fun getListSize(): Int
-
-    /**
      * 아이템 10개씩 호출
      */
     @Query("SELECT * FROM sample ORDER BY id ASC LIMIT 10 OFFSET (:page-1)*10")
