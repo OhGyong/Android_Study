d44923274e2822a103609f2b6f362486d67013d1
OhGyong <ogyong7143@gmail.com>
2023-03-28 10:16:24 +0900

    Room + Hilt 설정

--- Flow Sample/app/src/main/java/com/study/flowsample/data/SampleDatabase.kt
+++ Flow Sample/app/src/main/java/com/study/flowsample/data/SampleDatabase.kt
@@ -3,7 +3,7 @@ package com.study.flowsample.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Sample::class], version = 1, exportSchema = false)
+@Database(entities = [Sample::class], version = 1)
 abstract class SampleDatabase : RoomDatabase() {
     abstract fun sampleDao(): SampleDao
 }
