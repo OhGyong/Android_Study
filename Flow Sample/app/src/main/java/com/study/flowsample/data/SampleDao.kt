15cdb6b877088f34202076d7415773c2f59c0800
OhGyong <ogyong7143@gmail.com>
2023-03-28 10:16:24 +0900

    Room + Hilt 설정

--- Flow Sample/app/src/main/java/com/study/flowsample/data/SampleDao.kt
+++ Flow Sample/app/src/main/java/com/study/flowsample/data/SampleDao.kt
@@ -1,27 +1,10 @@
 package com.study.flowsample.data
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SampleDao {
     @Query("SELECT * FROM sample_db")
-    fun selectAll(): Flow<List<String>>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertSample(name: Sample)
-
-    @Query("DELETE FROM sample_db WHERE name=:name")
-    fun deleteSample(name: String)
-
-    @Query("UPDATE sample_db SET name=:changeName WHERE name=:originName")
-    fun updateSample(originName: String, changeName: String)
-
-    //////////////////
-
-    @Query("SELECT * FROM sample_db")
-    fun selectAll2(): List<String>
+    fun selectAll(): List<String>
 }
