92e8a972b327d7dda017cbbe2c14d3404999f244
OhGyong <ogyong7143@gmail.com>
2023-03-28 17:49:07 +0900

    StateFlow로 Select 후 UI 변경

--- Flow Sample/app/src/main/java/com/study/flowsample/MainActivity.kt
+++ Flow Sample/app/src/main/java/com/study/flowsample/MainActivity.kt
@@ -1,15 +1,39 @@
 package com.study.flowsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.study.flowsample.databinding.ActivityMainBinding
+import com.study.flowsample.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
+    private val mViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        mViewModel.callSelect()
+
+        CoroutineScope(Dispatchers.Main).launch {
+            mViewModel.sampleData.collect {
+                println("MainActivity1 $it")
+                mBinding.tvName.text = it.toString()
+
+            }
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            mViewModel.returnCallSelect().collect {
+                println("MainActivity2 $it")
+            }
+        }
     }
 }
