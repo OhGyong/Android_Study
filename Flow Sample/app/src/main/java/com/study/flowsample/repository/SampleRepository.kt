685f2b550066e445e398989d08980f2d5a9520e2
OhGyong <ogyong7143@gmail.com>
2023-03-29 15:36:45 +0900

    주석 추가

--- Flow Sample/app/src/main/java/com/study/flowsample/repository/SampleRepository.kt
+++ Flow Sample/app/src/main/java/com/study/flowsample/repository/SampleRepository.kt
@@ -11,22 +11,34 @@ import javax.inject.Singleton
 
 @Singleton
 class SampleRepository @Inject constructor(private val sampleDao: SampleDao) {
+    /**
+     * select 호출
+     */
     fun callSelect() : Flow<List<String>> {
         return sampleDao.selectAll()
     }
 
+    /**
+     * insert 호출
+     */
     fun callInsert(sample: Sample) {
         CoroutineScope(Dispatchers.IO).launch {
             sampleDao.insertSample(sample)
         }
     }
 
+    /**
+     * delete 호출
+     */
     fun callDelete(name: String) {
         CoroutineScope(Dispatchers.IO).launch {
             sampleDao.deleteSample(name)
         }
     }
 
+    /**
+     * update 호출
+     */
     fun callUpdate(originName: String, changeName: String) {
         CoroutineScope(Dispatchers.IO).launch {
             sampleDao.updateSample(originName, changeName)
