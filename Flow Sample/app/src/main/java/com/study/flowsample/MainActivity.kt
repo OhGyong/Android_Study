4b369dc00af03652c9e541ccd7f56fe58aad64ae
OhGyong <ogyong7143@gmail.com>
2023-03-29 14:33:37 +0900

    insert, delete, update 구현

--- Flow Sample/app/src/main/java/com/study/flowsample/MainActivity.kt
+++ Flow Sample/app/src/main/java/com/study/flowsample/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.study.flowsample
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.study.flowsample.data.Sample
 import com.study.flowsample.databinding.ActivityMainBinding
 import com.study.flowsample.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,26 +18,57 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
     private val mViewModel: MainViewModel by viewModels()
+    private lateinit var  inputMethodManager : InputMethodManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
+        inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
         mViewModel.callSelect()
 
         CoroutineScope(Dispatchers.Main).launch {
             mViewModel.sampleData.collect {
-                println("MainActivity1 $it")
                 mBinding.tvName.text = it.toString()
 
             }
         }
 
+        /**
+         * return으로 Select 결과 전달 받기
+         */
         CoroutineScope(Dispatchers.IO).launch {
             mViewModel.returnCallSelect().collect {
-                println("MainActivity2 $it")
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        mBinding.inInsert.btnInsert.setOnClickListener {
+            mViewModel.callInsert(Sample(mBinding.inInsert.etInsert.text.toString()))
+            btnSuccess(mBinding.inInsert.etInsert)
+        }
+
+        mBinding.inDelete.btnDelete.setOnClickListener {
+            mViewModel.callDelete(mBinding.inDelete.etDelete.text.toString())
+            btnSuccess(mBinding.inDelete.etDelete)
+        }
+
+        mBinding.inUpdate.btnUpdate.setOnClickListener {
+            mViewModel.callUpdate(
+                mBinding.inUpdate.etOrigin.text.toString(), mBinding.inUpdate.etChange.text.toString()
+            )
+            btnSuccess(mBinding.inUpdate.etOrigin)
+            btnSuccess(mBinding.inUpdate.etChange)
+        }
+    }
+
+    private fun btnSuccess(edt: EditText) {
+        inputMethodManager.hideSoftInputFromWindow(edt.windowToken, 0)
+        edt.text.clear()
     }
 }
