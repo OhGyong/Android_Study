2074b5f15484fc3d532e60031e23142eb517cfdf
OhGyong <ogyong7143@gmail.com>
2023-03-29 15:36:45 +0900

    주석 추가

--- Flow Sample/app/src/main/java/com/study/flowsample/MainActivity.kt
+++ Flow Sample/app/src/main/java/com/study/flowsample/MainActivity.kt
@@ -27,12 +27,15 @@ class MainActivity : AppCompatActivity() {
 
         inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
+        // db에 저장된 내용 조회
         mViewModel.callSelect()
 
+        /**
+         * select 결과 받기
+         */
         CoroutineScope(Dispatchers.Main).launch {
             mViewModel.sampleData.collect {
                 mBinding.tvName.text = it.toString()
-
             }
         }
 
@@ -48,16 +51,19 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
+        // insert 버튼
         mBinding.inInsert.btnInsert.setOnClickListener {
             mViewModel.callInsert(Sample(mBinding.inInsert.etInsert.text.toString()))
             btnSuccess(mBinding.inInsert.etInsert)
         }
 
+        // delete 버튼
         mBinding.inDelete.btnDelete.setOnClickListener {
             mViewModel.callDelete(mBinding.inDelete.etDelete.text.toString())
             btnSuccess(mBinding.inDelete.etDelete)
         }
 
+        // update 버튼
         mBinding.inUpdate.btnUpdate.setOnClickListener {
             mViewModel.callUpdate(
                 mBinding.inUpdate.etOrigin.text.toString(), mBinding.inUpdate.etChange.text.toString()
@@ -67,6 +73,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 버튼 클릭 후 키보드 내리고 텍스트 클리어
+     */
     private fun btnSuccess(edt: EditText) {
         inputMethodManager.hideSoftInputFromWindow(edt.windowToken, 0)
         edt.text.clear()
