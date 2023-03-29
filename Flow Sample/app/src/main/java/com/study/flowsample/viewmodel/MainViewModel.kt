3016a734a99624e295443cdde51f82b3c77eacd2
OhGyong <ogyong7143@gmail.com>
2023-03-29 15:36:45 +0900

    주석 추가

--- Flow Sample/app/src/main/java/com/study/flowsample/viewmodel/MainViewModel.kt
+++ Flow Sample/app/src/main/java/com/study/flowsample/viewmodel/MainViewModel.kt
@@ -13,9 +13,12 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(private val sampleRepository: SampleRepository) : ViewModel() {
 
     private var _sampleData = MutableStateFlow<List<String>>(emptyList())
-//    val sampleData: StateFlow<List<String>> = _sampleData
     val sampleData = _sampleData.asStateFlow()
+//    val sampleData: StateFlow<List<String>> = _sampleData
 
+    /**
+     * select 호출, 캡슐화
+     */
     fun callSelect() {
         viewModelScope.launch {
             sampleRepository.callSelect().collectLatest { it ->
@@ -24,6 +27,9 @@ class MainViewModel @Inject constructor(private val sampleRepository: SampleRepo
         }
     }
 
+    /**
+     * select 호출, return으로 반환
+     */
     fun returnCallSelect(): StateFlow<List<String>> {
         // stateIn으로 StateFlow로 변환
         return sampleRepository.callSelect().stateIn(
@@ -31,14 +37,23 @@ class MainViewModel @Inject constructor(private val sampleRepository: SampleRepo
         )
     }
 
+    /**
+     * insert 호출
+     */
     fun callInsert(sample: Sample) {
         sampleRepository.callInsert(sample)
     }
 
+    /**
+     * delete 호출
+     */
     fun callDelete(name: String) {
         sampleRepository.callDelete(name)
     }
 
+    /**
+     * update 호출
+     */
     fun callUpdate(originName: String, changeName: String) {
         sampleRepository.callUpdate(originName, changeName)
     }
