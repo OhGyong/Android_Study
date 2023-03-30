f24b5b5ca413916ef1d0f50b6488a3f602de5dd3
OhGyong <ogyong7143@gmail.com>
2023-03-30 13:56:39 +0900

    Flow와 StateFlow에 대한 비교 테스트

--- Flow Sample/app/src/main/java/com/study/flowsample/MainActivity.kt
+++ Flow Sample/app/src/main/java/com/study/flowsample/MainActivity.kt
@@ -31,19 +31,61 @@ class MainActivity : AppCompatActivity() {
         mViewModel.callSelect()
 
         /**
-         * select 결과 받기
+         * 1-1.
+         * - Dao에서 Flow로 반환
+         * - ViewModel에서 StateFlow로 처리 (캡슐화)
+         * 이전 데이터와 비교하여 같을 경우 collect 되지 않음. 새로운 데이터일 경우에만 collect
          */
         CoroutineScope(Dispatchers.Main).launch {
             mViewModel.sampleData.collect {
+                println("callSelect1 $it")
                 mBinding.tvName.text = it.toString()
             }
         }
 
         /**
+         * 1-2.
          * return으로 Select 결과 전달 받기
          */
         CoroutineScope(Dispatchers.IO).launch {
             mViewModel.returnCallSelect().collect {
+            }
+        }
+
+        /**
+         * 2.
+         * - Dao에서 List로 반환
+         * - Flow로 받음
+         * 첫 데이터에만 반응, 값의 변화에 대해 반응 없음
+         */
+        CoroutineScope(Dispatchers.IO).launch {
+            mViewModel.callSelect2().collect {
+                println("callSelect2 $it")
+            }
+        }
+
+        /**
+         * 3.
+         * - Dao에서 List로 반환
+         * - StateFlow로 받음
+         *  첫 데이터에만 반응, 값의 변화에 대해 반응 없음
+         */
+        CoroutineScope(Dispatchers.IO).launch {
+            mViewModel.callSelect3().collect {
+                println("callSelect3 $it")
+            }
+        }
+
+        /**
+         * 4.
+         * - Dao에서 Flow로 반환
+         * - Flow로 받음
+         * 값의 변화에 따라 호출됨
+         * 이전 데이터에 상관없이 모든 값을 발행
+         */
+        CoroutineScope(Dispatchers.IO).launch {
+            mViewModel.callSelect4().collect {
+                println("callSelect4 $it")
             }
         }
     }
