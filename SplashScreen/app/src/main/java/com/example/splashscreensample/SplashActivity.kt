f8e94969d86c3792ca3f989d434c145dde89fffe
OhGyong <ogyong7143@gmail.com>
2023-04-12 22:42:05 +0900

    커스텀 스플래시 작업

--- SplashScreen/app/src/main/java/com/example/splashscreensample/SplashActivity.kt
+++ SplashScreen/app/src/main/java/com/example/splashscreensample/SplashActivity.kt
@@ -3,9 +3,8 @@ package com.example.splashscreensample
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
 
 class SplashActivity: AppCompatActivity() {
 
@@ -13,14 +12,10 @@ class SplashActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 2000)
-
-//
-//        startActivity(Intent(this, MainActivity::class.java))
-//        finish()
+        }, 1500)
     }
 
 }
