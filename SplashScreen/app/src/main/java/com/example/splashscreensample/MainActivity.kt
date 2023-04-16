452f2055b603741ef86a09f7428e61ddd0c6199e
OhGyong <ogyong7143@gmail.com>
2023-04-16 13:47:23 +0900

    코드 정리

--- SplashScreen/app/src/main/java/com/example/splashscreensample/MainActivity.kt
+++ SplashScreen/app/src/main/java/com/example/splashscreensample/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.splashscreensample
 
-import android.animation.Animator
 import android.animation.ObjectAnimator
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,13 +8,12 @@ import android.view.ViewTreeObserver
 import android.view.animation.AnticipateInterpolator
 import androidx.activity.viewModels
 import androidx.core.animation.doOnEnd
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
 
     private val mViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+//        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -34,36 +31,18 @@ class MainActivity : AppCompatActivity() {
             }
         )
 
-//        splashScreen.setOnExitAnimationListener { splashScreenView ->
-//            ObjectAnimator.ofFloat(
-//                splashScreenView,
-//                View.TRANSLATION_Y,
-//                0f,
-//                -splashScreenView.height.toFloat()
-//            ).run {
-//                interpolator = AnticipateInterpolator()
-//                duration = 1000
-//                doOnEnd { splashScreenView.remove() }
-//                start()
-//                addListener(object : Animator.AnimatorListener{
-//                    override fun onAnimationStart(p0: Animator) {
-//                    }
-//
-//                    override fun onAnimationEnd(p0: Animator) {
-//                        val intent = Intent(applicationContext, MainActivity::class.java)
-//                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-//                        startActivity(intent)
-//                        finish()
-//                    }
-//
-//                    override fun onAnimationCancel(p0: Animator) {
-//                    }
-//
-//                    override fun onAnimationRepeat(p0: Animator) {
-//                    }
-//
-//                })
-//            }
-//        }
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.height.toFloat()
+            ).run {
+                interpolator = AnticipateInterpolator()
+                duration = 1000
+                doOnEnd { splashScreenView.remove() }
+                start()
+            }
+        }
     }
 }
