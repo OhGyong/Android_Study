ffaf4388e76bcbcdc77e491fa1709278d0224a74
O_Gyong <ogyong7143@gmail.com>
2025-07-11 18:02:16 +0900

    [Calendar by Kizitonwose] - 캘린더 step3 컴포즈 변환중

---
+++ Calendar by Kizitonwose/app/src/main/java/com/study/calendarbykizitonwose/MainScreen.kt
@@ -0,0 +1,45 @@
+package com.study.calendarbykizitonwose
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.kizitonwose.calendar.compose.HorizontalCalendar
+import com.kizitonwose.calendar.compose.rememberCalendarState
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import java.time.YearMonth
+
+@Composable
+fun MainScreen() {
+    val currentMonth = remember { YearMonth.now() }
+    val startMonth = remember { currentMonth.minusMonths(100) } // Adjust as needed
+    val endMonth = remember { currentMonth.plusMonths(100) } // Adjust as needed
+    val firstDayOfWeek = remember { firstDayOfWeekFromLocale() } // Available from the library
+
+    val state = rememberCalendarState(
+        startMonth = startMonth,
+        endMonth = endMonth,
+        firstVisibleMonth = currentMonth,
+        firstDayOfWeek = firstDayOfWeek
+    )
+
+    HorizontalCalendar(
+        state = state,
+        dayContent = { Day(it) }
+    )
+}
+
+@Composable
+fun Day(day: CalendarDay) {
+    Box(
+        modifier = Modifier
+            .aspectRatio(1f), // This is important for square sizing!
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = day.date.dayOfMonth.toString())
+    }
+}
--- Calendar by Kizitonwose/app/src/main/java/com/study/calendarbykizitonwose/MonthViewContainer.kt
+++
@@ -1,9 +0,0 @@
-package com.study.calendarbykizitonwose
-
-import android.view.View
-import android.view.ViewGroup
-import com.kizitonwose.calendar.view.ViewContainer
-
-class MonthViewContainer(view: View) : ViewContainer(view) {
-    val titlesContainer = view as ViewGroup
-}
